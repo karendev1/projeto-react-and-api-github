fd148d2b8844cb8843871e6e86a9f9f5fc9f7959
Karen Christina <karen.nascimento.developer@gmail.com>
2023-03-29 11:22:08 -0300

    feat: create component home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Search from "../components/search";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <Search />
+  </div>;
 };
 
 export default Home;
